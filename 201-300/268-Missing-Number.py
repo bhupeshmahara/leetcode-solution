148273d3850e291ffc555cb78b138342f51bdbeb
bhupeshmahara <bhupeshmahara@gmail.com>
2023-08-08 23:17:19 +0530

    updated missing number

---
+++ 201-300/268-Missing-Number.py
@@ -0,0 +1,9 @@
+from typing import List
+
+# time O(n)
+# space O(n)
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return n * (n + 1) // 2 - sum(nums)
